048acf8875f8522733f5119acf6e52299697df75
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-10-18 11:07:38 -0700

    ✨feat: update package exports

--- src/packages/supernova/index.ts
+++ src/packages/supernova/index.ts
@@ -1,2 +1,6 @@
-export * from './src/atoms'
+'use client'
+
+export * from './src/components/atoms'
+export { css, keyframes, cx, merge } from './src/utils/css'
+export { default as GlobalStyles } from './src/styles/global-styles'
 export { AnimatePresence, motion } from 'framer-motion'
