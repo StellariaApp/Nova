59514eb09722f73e271f260a42bf87dcb7168edf
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-12-12 17:31:26 -0700

    ✨feat: update package dependencies and file paths

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
+import { Card,Button,Code } from "@stellaria/nova";
 import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
 
 function Gradient({
   conic,
