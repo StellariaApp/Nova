f469610d8484714039f7faad6df0cb494cfd4fc0
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-12-15 20:56:54 -0700

    refactor

---
+++ core/components/atoms/Button/index.tsx
@@ -0,0 +1,8 @@
+
+export type ButtonProps = {}
+
+export const Button = (props: ButtonProps) => {
+    return (
+        <div>Button Nova</div>
+    )
+}
--- packages/nova/src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './atoms';
-// export * from './complex';
-// export * from './templates';
