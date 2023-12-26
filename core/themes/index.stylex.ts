716b7fec4a9a3739980a8b098b3cddd663cfaa37
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-12-26 03:31:52 -0700

    :art: style(core/themes): update font sizes in index.stylex.ts

--- core/themes/index.stylex.ts
+++ core/themes/index.stylex.ts
@@ -91,26 +91,29 @@ export const sizesRaw = {
   xxsmall: "0.25rem",
   xsmall: "0.5rem",
   small: "0.75rem",
-  button: "0.875rem",
-  medium: "1rem",
-  large: "1.25rem",
-  xlarge: "1.5rem",
-  xxlarge: "1.75rem",
+  regular: "0.875rem",
+  large: "1rem",
+  xl: "1.125rem",
+  xlarge: "1.25rem",
+  xxlarge: "1.5rem",
 };
 
 export const spacingRaw = {
   xsmall: "0.5rem",
   small: "0.75rem",
-  medium: "1rem",
-  large: "1.25rem",
-  xlarge: "1.5rem",
-  xxlarge: "1.75rem",
+  regular: "0.875rem",
+  large: "1rem",
+  xlarge: "1.25rem",
+  xxlarge: "1.5rem",
 };
 export const borderRadiusRaw = {
+  xxsmall: "0.0625rem",
+  xsmall: "0.125rem",
   small: "0.25rem",
-  medium: "0.5rem",
+  regular: "0.5rem",
   large: "0.75rem",
   xlarge: "1rem",
+  xxlarge: "1.5rem",
 };
 
 export const fontsRaw = {
